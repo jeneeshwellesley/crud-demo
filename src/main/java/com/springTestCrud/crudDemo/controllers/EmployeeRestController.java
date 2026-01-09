cdf255c1a2583d515110c786929d69086ace13a5
Jeneesh Wellesley <jeneesh86@gmail.com>
2026-01-09 17:07:34 +0530

    Added new methods in the rest controller for CRUD operations

--- src/main/java/com/springTestCrud/crudDemo/controllers/EmployeeRestController.java
+++ src/main/java/com/springTestCrud/crudDemo/controllers/EmployeeRestController.java
@@ -4,21 +4,22 @@ package com.springTestCrud.crudDemo.controllers;
 import com.springTestCrud.crudDemo.entities.Employee;
 import com.springTestCrud.crudDemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController{
     private EmployeeService employeeService;
+    private JsonMapper jsonMapper;
 
     @Autowired
-    public EmployeeRestController (EmployeeService employeeService){
+    public EmployeeRestController (EmployeeService employeeService, JsonMapper jsonMapper) {
         this.employeeService = employeeService;
+        this.jsonMapper = jsonMapper;
     }
 
     @GetMapping("/employees")
@@ -26,9 +27,42 @@ public class EmployeeRestController{
         return employeeService.getAllEmployees();
     }
 
-    @GetMapping("/employee/{employeeId}")
+    @GetMapping("/employees/{employeeId}")
     public Employee getEmployeeById(@PathVariable int employeeId){
         Employee employee = employeeService.findById(employeeId);
         return employee;
     }
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee){
+        employee.setId(0);
+        Employee dbEmployee = employeeService.addEmployee(employee);
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        Employee dbEmployee = employeeService.updateEmployee(employee);
+        return dbEmployee;
+    }
+
+    @PatchMapping("/employees/{employeeId}")
+    public Employee updateEmployeeFields(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload){
+
+        Employee dbEmployee = employeeService.findById(employeeId);
+
+        Employee patchedEmployee = jsonMapper.updateValue(dbEmployee, patchPayload);
+        Employee dEmployee = employeeService.updateEmployee(patchedEmployee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee dbEmployee = employeeService.findById(employeeId);
+        return employeeService.deleteEmployee(employeeId);
+    }
+
+
+
+
 }
