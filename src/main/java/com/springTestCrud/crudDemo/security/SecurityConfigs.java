fc36af4b04f4ccc5f924f76772fa3aa8f6a53ed7
jeneeshwellesley <jeneesh86@gmail.com>
2026-01-10 23:04:22 +0530

    Added UserDetailsManager to connect with db for authentication

--- src/main/java/com/springTestCrud/crudDemo/security/SecurityConfigs.java
+++ src/main/java/com/springTestCrud/crudDemo/security/SecurityConfigs.java
@@ -5,14 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfigs {
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /*
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 
@@ -31,19 +38,21 @@ public class SecurityConfigs {
         UserDetails jeneesh = User.builder()
                 .username("jeneesh")
                 .password("{noop}123@")
-                .roles("EMPLOYEE","MANAGER")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
 
 
         return new InMemoryUserDetailsManager(john, preethi, jeneesh);
     }
 
+     */
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasAnyRole("EMPLOYEE", "MANAGER","ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasAnyRole("EMPLOYEE","MANAGER","ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/employees/**").hasAnyRole("EMPLOYEE", "MANAGER","ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasAnyRole("MANAGER","ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasAnyRole("MANAGER","ADMIN")
